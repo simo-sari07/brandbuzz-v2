40cbcc57b5c674cc8aecf442bb1c6e432599a6c0
simo-sari07 <simosari100@gmail.com>
2025-04-08 17:51:21 +0100

    maste

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 function Logo() {
   return (
     <div className="relative w-40 h-12">
-      <img src="/logox.png" alt="Company Logo" className="object-contain w-full h-full" />
+      <img src="/logox.png" alt="Brandbuzz Logo" className="object-contain w-full h-full" />
     </div>
   )
 }
