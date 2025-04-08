8fcbd93703975f86ef21872c18eaf49f12beefa7
simo-sari07 <simosari100@gmail.com>
2025-04-08 23:20:56 +0100

    new updatee

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-"use client"
+"use client";
 
 // Import components
-import Header from "./components/Header"
-import HeroSection from "./components/HeroSection"
-import ServicesSection from "./components/ServicesSection"
-import ProcessSection from "./components/ProcessSection"
-import CreativeShowcase from "./components/CreativeShowcase"
-import NosProjects from "./components/NosProjects"
-import ServicesSEO from "./components/ServicesSEO"
-import CreationWeb from "./components/CreationWeb"
-import TestimonialsSection from "./components/TestimonialsSection"
-import CtaSection from "./components/CtaSection"
-import Footer from "./components/Footer"
-import Whatsapp from "./components/Whatsapp"
-import React from "react"
-import VideoShowcase from "./components/VideoShowcase"
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import ServicesSection from "./components/ServicesSection";
+import ProcessSection from "./components/ProcessSection";
+import CreativeShowcase from "./components/CreativeShowcase";
+import NosProjects from "./components/NosProjects";
+import ServicesSEO from "./components/ServicesSEO";
+import CreationWeb from "./components/CreationWeb";
+import TestimonialsSection from "./components/TestimonialsSection";
+import CtaSection from "./components/CtaSection";
+import Footer from "./components/Footer";
+import Whatsapp from "./components/Whatsapp";
+import React from "react";
+import VideoShowcase from "./components/VideoShowcase";
+import Faq from "./components/Faq";
 function App() {
   return (
     <div className="min-h-screen bg-white">
@@ -31,15 +32,14 @@ function App() {
         <ServicesSEO />
         <NosProjects />
         <CreativeShowcase />
-        <VideoShowcase/>
+        <VideoShowcase />
+        <Faq />
         <TestimonialsSection />
         <CtaSection />
       </main>
-
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
